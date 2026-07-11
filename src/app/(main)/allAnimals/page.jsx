11c157e2af9912ccca0b9b43519e7d9639792bf3
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-07-11 03:18:41 +0300

    Design All Animals card and add them into all animals page

--- src/app/(main)/allAnimals/page.jsx
+++ src/app/(main)/allAnimals/page.jsx
@@ -1,9 +1,16 @@
+import AnimalCard from '@/components/Cards/AnimalCard';
+import { allAnimalsData } from '@/lib/AnimalsData';
 import React from 'react';
 
-const AllAnimalsPage = () => {
+const AllAnimalsPage = async() => {
+    const allAnimals= await allAnimalsData();
+    console.log(allAnimals )
+
     return (
-        <div>
-            <h2>All animal</h2>
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 md:gap-6 mt-16 p-2'>
+                {allAnimals.map(animal => (<AnimalCard key={animal.id} animal={animal}></AnimalCard>))}
+            </div>
         </div>
     );
 };
--- src/components/Shared/Featured.jsx
+++ src/components/Shared/Featured.jsx
@@ -4,8 +4,8 @@ import AnimalCard from '../Cards/AnimalCard';
 
 const Featured = async() => {
     const animals = await allAnimalsData();
-    const featuredAnimals = animals.slice(0,4);
-    console.log(featuredAnimals)
+    const allAnimals = animals.slice(0,4);
+    
 
     return (
         <div className='container mx-auto my-14 px-3.5'>
@@ -15,7 +15,7 @@ const Featured = async() => {
                     <p className="font-medium text-md lg:text-lg text-gray-500">Browse our most popular and high-quality Qurbani animals. From local breeds to premium livestock, <br/>find the perfect animal that meets your needs and budget.</p>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 my-5'>
-                    {featuredAnimals.map((animal) => (<AnimalCard key={animal.id} animal={animal}></AnimalCard>))}
+                    {allAnimals.map((animal) => (<AnimalCard key={animal.id} animal={animal}></AnimalCard>))}
 
                 </div>
             </div>
